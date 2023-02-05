78f80687c6e809f43f7b25c94e1981e54dfa97a1
Vitalii Nikolaienko <vnickolaenko@gmail.com>
2023-02-05 23:01:59 +0300

    fix bugs

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -8,88 +8,72 @@ const Users = () => {
   };
   const renderPhrase = (number) => {
     if (!number) {
-      return (
-        <h1>
-          <span className="badge bg-danger">Никто с тобой не тусанет</span>
-        </h1>
-      );
+      return <span className="badge bg-danger">Никто с тобой не тусанет</span>;
     }
 
     if ([2, 3, 4].includes(number % 10) && ![12, 13, 14].includes(number)) {
       return (
-        <h1>
-          <span className="badge bg-primary">
-            {number} человека тусанут с тобой сегодня
-          </span>
-        </h1>
+        <span className="badge bg-primary">
+          {number} человека тусанут с тобой сегодня
+        </span>
       );
     } else {
       return (
-        <h1>
-          <span className="badge bg-primary">
-            {number} человек тусанет с тобой сегодня
-          </span>
-        </h1>
+        <span className="badge bg-primary">
+          {number} человек тусанет с тобой сегодня
+        </span>
       );
     }
-  };
-  const renderTable = () => {
-    return (
-      Boolean(users.length) && (
-        <>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => {
-                return (
-                  <>
-                    <tr>
-                      <td>{user.name}</td>
-                      <td>
-                        {user.qualities.map((qualitie) => {
-                          return (
-                            <span className={"badge m-1 bg-" + qualitie.color}>
-                              {qualitie.name}
-                            </span>
-                          );
-                        })}
-                      </td>
-                      <td>{user.profession.name}</td>
-                      <td>{user.completedMeetings}</td>
-                      <td>{user.rate} /5</td>
-                      <td>
-                        <button
-                          key={user._id}
-                          className="btn btn-danger"
-                          onClick={() => handleDelete(user._id)}
-                        >
-                          delete
-                        </button>
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
-        </>
-      )
-    );
   };
 
   return (
     <>
-      {renderPhrase(users.length)}
-      {renderTable()}
+      <h1 className="m-1">{renderPhrase(users.length)}</h1>
+      {Boolean(users.length) && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <>
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>
+                      {user.qualities.map((qualitie) => {
+                        return (
+                          <span className={"badge m-1 bg-" + qualitie.color}>
+                            {qualitie.name}
+                          </span>
+                        );
+                      })}
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate} /5</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(user._id)}
+                      >
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
